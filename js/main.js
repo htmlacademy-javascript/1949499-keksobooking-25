6a53f444600d23c0596e33e2897e2ff875da3533
lexusegorov <alexey.egorov.job@gmail.com>
2022-02-20 09:20:22 +0700

    Исправил ошибки линтера

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function getRand(min, max, fix){
-    if(min < 0 || min >= max || fix < 0 || fix > 100){
-        console.log("Диапазон от 0 до n, где n > 0!"); 
-        console.log("fix от 0, до 100!"); 
-        return;
-    }
-    const randResult = Math.random() * (max - min) + min;
-    if(fix){
-        return Number(randResult.toFixed(fix));
-    }
-    return Math.round(randResult);
-};
+  if(min < 0 || min >= max || fix < 0 || fix > 100){
+    return -1; //будем считать, что это код ошибки
+  }
+  const randResult = Math.random() * (max - min) + min;
+  if(fix){
+      return Number(randResult.toFixed(fix));
+  }
+  return Math.round(randResult);
+}
+
+getRand(1, 10, 5);
