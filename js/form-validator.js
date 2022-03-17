27b84cba1cd34615a2f9dfff1431c5efe1962def
lexusegorov <alexey.egorov.job@gmail.com>
2022-03-17 22:31:56 +0700

    Добавлена валидация данных часть 1

--- js/form-validator.js
+++ js/form-validator.js
@@ -11,24 +11,24 @@ const pristine = new Pristine(adForm, {
   errorTextClass: 'ad-form__error',
 });
 
-function validateTitle(value){
-  return value.length >=30 && value.length <= 100;
+function validateTitle(value) {
+  return value.length >= 30 && value.length <= 100;
 }
 
-function validatePrice(value){
+function validatePrice(value) {
   return value <= 100000;
 }
 
-function getMaxGuests(value){
-  if(value > 3){
+function getMaxGuests(value) {
+  if (value > 3) {
     return 0;
   }
   return value;
 }
 
-function validateGuests(value){
+function validateGuests(value) {
   const max = getMaxGuests(adForm.querySelector('#room_number').value);
-  if(max === 0 || Number(value) === 0){
+  if (max === 0 || Number(value) === 0) {
     return max === Number(value);
   }
   return max >= value && value !== 0;
